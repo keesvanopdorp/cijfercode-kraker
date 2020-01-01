59ce088b695a0c869f389697e96e87179398f859
kees <keesjrvanopdorp@live.nl>
2020-01-01 17:14:25 +0100

    made the basic version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from '@angular/forms';
+
+import {WordService} from './shared/word.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cijfercode-kraker';
+export class AppComponent implements OnInit {
+  letterForm: FormGroup;
+  words: any;
+  constructor(private fb: FormBuilder, private wordService: WordService) {}
+
+  ngOnInit(): void {
+    this.letterForm = this.fb.group({
+     value: ['', [
+       Validators.required
+     ]]
+    });
+    this.wordService.getWordList();
+  }
+
+  submitHandler() {
+    let str = this.letterForm.value.value;
+    let count = 0;
+    let regex: string | RegExp = '^';
+    let previousChar = '';
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) !== '*') {
+        if (previousChar === '*') {
+          regex += `{${count}}`;
+          regex += str.charAt(i);
+          previousChar = '';
+          count = 0;
+        } else {
+          regex += str.charAt(i);
+        }
+      } else {
+        if (count === 0 ) {
+          regex += '[a-zA-Z]';
+          count += 1;
+          previousChar = '*';
+        } else {
+          count += 1;
+          previousChar = '*';
+        }
+      }
+    }
+    regex += '$';
+    regex = new RegExp(regex);
+    this.words = this.wordService.filter(regex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FooterComponent } from './layout/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TimesPipe } from './shared/times.pipe';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent, TimesPipe],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
