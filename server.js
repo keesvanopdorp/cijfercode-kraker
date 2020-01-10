03c8b399565867f20505c3eee695d42792e9279a
kees <keesjrvanopdorp@live.nl>
2020-01-10 10:41:57 +0100

    release 2.3.0: - added forced https

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ let server = require('http').Server(app);
 app.use(express.static('docs'));
 
 app.get('/', function(req,res) {
-  res.sendFile(__dirname + '/docs/index.html');
+  console.log(`secure: ${!req.secure}`);
+  if(!req.secure){
+    return res.redirect('https://' + req.header.host + req.path);
+  }else {
+    res.sendFile(__dirname + '/docs/index.html');
+  }
 });
 
 // Start the app by listening on the default Heroku port
